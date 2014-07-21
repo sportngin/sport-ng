876ceebb3d5c9a5bf79a5af1f6c0c2afbf1e59da
Jesse Houchins <jesse.houchins@tstmedia.com>
2014-07-21 15:36:48 -0500

    update i18next for tests

--- karma.conf.js
+++ karma.conf.js
@@ -20,6 +20,7 @@ module.exports = function(config) {
       'bower_components/angular-resource/angular-resource.js',
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
+      'bower_components/i18next/i18next.js',
       'bower_components/i18ng/i18ng.js',
       'bower_components/tether/tether.js',
       'bower_components/underscore/underscore.js',
