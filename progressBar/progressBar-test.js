ef2878d8366fe90a25417071e166a61d82a64b9d
Jesse Houchins <jesse.houchins@tstmedia.com>
2014-07-21 15:08:30 -0500

    test updates

--- progressBar/progressBar-test.js
+++ progressBar/progressBar-test.js
@@ -1,19 +1,23 @@
 
 describe('ProgressBar', function() {
 
+  var directive
+  var MockDirective
+
   beforeEach(module('sport.ng'))
 
-  // beforeEach(inject(function(ResourceManager) {
-  //   api = ResourceManager(resourceConfig)
-  //   resSpy.calls.reset()
-  // }))
+  beforeEach(inject(function(_MockDirective_) {
+    MockDirective = _MockDirective_
+  }))
 
-  it('should use percentage if passed in', function() {
-    expect(false).toBe(true)
+  it('should use `percent` if passed in', function() {
+    directive = new MockDirective('<div progress-bar percent="73"></div>')
+    expect(directive.$scope.progress()).toBe(73)
   })
 
-  it('should calculate percentage if not passed in', function() {
-    expect(false).toBe(true)
+  it('should calculate `percent` if not passed in', function() {
+    directive = new MockDirective('<div progress-bar total="10" completed="4"></div>')
+    expect(directive.$scope.progress()).toBe(40)
   })
 
 })
