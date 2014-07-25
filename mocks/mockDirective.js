4a0920ada6653bd0489136f2a91f747708d51d3c
Jesse Houchins <jesse.houchins@tstmedia.com>
2014-07-25 12:10:23 -0500

    remove unused code

--- mocks/mockDirective.js
+++ mocks/mockDirective.js
@@ -1,17 +1,15 @@
 'use strict'
 
 angular.module('sport.ng')
-  .factory('MockDirective', function($compile, $location, $rootScope) {
+  .factory('MockDirective', function($compile, $rootScope) {
 
     function MockDirective(tmpl, parentScope) {
-      this.$location = $location
       this.$parentScope = angular.extend($rootScope.$new(), parentScope|| {})
       if (this.defaultTemplate = tmpl) this.compile()
     }
 
     MockDirective.prototype.compile = function(tmpl) {
       if (!tmpl && !this.defaultTemplate) throw "Cannot compile mock directive without a template."
-      this.$parentScope.mockParentScope = true
       this.$element = $compile(tmpl || this.defaultTemplate)(this.$parentScope)
       this.$parentScope.$digest()
       this.$scope = this.$element.isolateScope() || this.$element.scope()
