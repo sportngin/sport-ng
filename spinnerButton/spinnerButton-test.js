a3e3843b3bf4ca95d6c695ad9ade72f8638e1ee4
Bob Black <bob.black@sportngin.com>
2014-08-01 14:17:52 -0500

    Add spinner-button

---
+++ spinnerButton/spinnerButton-test.js
@@ -0,0 +1,62 @@
+;(function() {
+'use strict'
+
+describe('spinnerButon', function () {
+  var $compile
+  var $q
+  var $parentScope
+  var elm
+  var click
+
+  beforeEach(angular.mock.module('sport.ng'))
+  beforeEach(angular.mock.module('sport.ng.templates'))
+
+  beforeEach(inject(function(_$compile_, _$httpBackend_, $rootScope, $templateCache, _$q_) {
+    $compile = _$compile_
+    $q = _$q_
+    $parentScope = $rootScope.$new()
+    click = function() { }
+  }))
+
+  function compileDirective(tpl) {
+    if (!tpl) {
+      tpl = "<button spinner-button spinner-click='click()'></div>"
+    }
+    elm = $compile(tpl)($parentScope)
+    $parentScope.$digest()
+    $parentScope.click = click
+  }
+
+  it('should initialize', function() {
+    compileDirective()
+  })
+
+  it('should call spinner-click handler', function() {
+    compileDirective()
+    spyOn($parentScope, 'click').and.callThrough()
+    elm.triggerHandler('click')
+    expect($parentScope.click.calls.count()).toEqual(1)
+  })
+
+  describe('spinner-click #promise', function() {
+    var dfd
+    beforeEach(function() {
+      dfd = $q.defer()
+      click = function() { return dfd.promise }
+      compileDirective()
+      elm.triggerHandler('click')
+    })
+
+    it('should set spinning when promise is returned from spinner-click', function() {
+      expect(elm.hasClass('spinning')).toBe(true)
+    })
+
+    it('should clear spinning when promise resolves', function() {
+      dfd.resolve()
+      $parentScope.$digest()
+      expect(elm.hasClass('spinning')).toBe(false)
+    })
+  })
+})
+
+})();
