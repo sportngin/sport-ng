2a3b692880c13fc47886d659d0cec9a48cc1ef6b
Bob Black <bob.black@sportngin.com>
2014-08-20 12:34:06 -0500

    Update spinnerButton to latest in sport_admin

--- spinnerButton/spinnerButton.js
+++ spinnerButton/spinnerButton.js
@@ -19,34 +19,39 @@ angular.module('sport.ng')
 .directive('spinnerButton', function($parse) {
   return {
     restrict: 'AE',
-    //replace: true,
     transclude: true,
-    //template: '<button ng-class="{ spinning: spinning }"><div ng-transclude></div><span class="spinner"></span></button>',
     template: '<div ng-transclude></div><span class="spinner"></span>',
     link: function(scope, elem, attrs) {
-      var spinning
-      var getSpinning
-      if (attrs.spinning !== undefined) {
-        getSpinning = $parse(attrs.spinning)
-      }
-      scope.$watch(function() {
-        if (getSpinning) {
-          return getSpinning(scope) || spinning
+      var spinningBool
+      var spinningFn
+      var oldProp
+      var oldAttr
+
+      if ('spinning' in attrs) spinningFn = $parse(attrs.spinning)
+
+      function spinningValue() { return spinningFn && spinningFn(scope) || spinningBool }
+
+      scope.$watch(spinningValue, function(spinning) {
+        elem.toggleClass('spinning', !!spinning)
+        if (spinning) {
+          oldProp = elem.prop('disabled')
+          oldAttr = elem.attr('disabled')
+          elem.attr('disabled', true)
         }
-        return spinning
-      }, function(spinning) {
-        if (spinning) elem.addClass('spinning')
-        else elem.removeClass('spinning')
+        else {
+          if (!oldAttr) elem.removeAttr('disabled')
+          if (!oldProp) elem.removeProp('disabled')
+        }
       })
 
-      var spinnerClick = $parse(attrs.spinnerClick)
+      // This will return a noop method if spinnerClick isn't set
+      var clickFn = $parse(attrs.spinnerClick)
       elem.on('click', function(event) {
         scope.$apply(function() {
-          var clickResult = spinnerClick(scope, { $event: event })
-          if (clickResult && clickResult.then) {
-            spinning = true
-            var clearSpinning = function() { spinning = false }
-            clickResult.then(clearSpinning, clearSpinning)
+          var clickPromise = clickFn(scope, { $event: event })
+          if (clickPromise && clickPromise.finally) {
+            spinningBool = true
+            clickPromise.finally(function(){ spinningBool = false })
           }
         })
       })
