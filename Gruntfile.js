df469e4d8221de93b1428d502f4e18908f9491f0
clado <annelies.odermann@gmail.com>
2015-01-29 15:01:49 -0600

    Dev doesn't need any of this stuff. Let's see if ci needs it.

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,27 +7,6 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
 
-    "bower-install": {
-
-      target: {
-
-        // Point to the files that should be updated when
-        // you run `grunt bower-install`
-        src: [
-
-        ],
-
-        // Optional:
-        // ---------
-        cwd: '',
-        dependencies: false,
-        devDependencies: true,
-        exclude: [],
-        fileTypes: {},
-        ignorePath: ''
-      }
-    },
-
     karma: {
       unit: {
         configFile: 'karma.conf.js',
@@ -37,16 +16,11 @@ module.exports = function(grunt) {
 
   })
 
-  // Default task.
-  // put require before handlebars because require wipes the dir
-
-  grunt.registerTask('setup',   ['npm-install', 'bower-install'])
   grunt.registerTask('test',    ['karma'])
   grunt.registerTask('ci',      ['test'])
+  grunt.registerTask('default', ['karma'])
 
   // load the grunt task plugins
-  grunt.loadNpmTasks('grunt-bower-install')
-  grunt.loadNpmTasks('grunt-npm-install');
   grunt.loadNpmTasks('grunt-karma')
 
 }
